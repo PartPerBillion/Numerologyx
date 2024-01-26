3364e7fb4f02c281056475533385c801e2a627eb
Srihari P <92159097+PartPerBillion@users.noreply.github.com>
2024-01-26 14:51:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,11 +109,12 @@ def numerology(x):
     print('Sum - ',sumx)
     number = sumx
     while len(str(number))>1:
+        st.caption(f'Sum - {number}')
         sumx = 0
         for i in str(number):
             sumx+=int(i)
             number = sumx
-            st.caption(f'Sum - {number}')
+            
     st.divider()
     if number>0:
         st.subheader(f'Numerology number - {number}')
